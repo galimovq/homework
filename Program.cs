9b2e28686a4282bdf28942a810683d5effc80f35
galimovq <galimovq23@gmail.com>
2019-04-10 22:25:55 +0500

    homework_1

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace project {
+	class MainClass {
+		public static void Main (string[] args) {
+			
+			Console.WriteLine ("Таблица умножения");
+
+			for (int i = 0; i < 9; i++) {
+				for (int j = 0; j < 9; j++) {
+					Console.Write ("\t" + ((i + 1) * (j + 1)));
+				}
+				Console.WriteLine ();
+			}
+
+			Console.ReadKey ();
+
+		}
+	}
+}
